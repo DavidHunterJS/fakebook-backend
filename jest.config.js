7729c921922b0e354ade3af199c9805018f737b4
Lazer Beem <lazer@trippy.wtf>
2025-06-20 17:19:03 -0700

    removed hardcoded jwt_secrets from jest.config

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,12 @@
-process.env.JWT_SECRET='12e7b872b07f96f6c3b3e02903767bfcf2fe699b8ce05d72e4663f6c5750a5ac'
-process.env.NODE_ENV = 'test';
 // jest.config.js
 module.exports = {
+  // 'dotenv/config' should be sufficient here to load from .env files during local development.
+  // For CI, Jenkins will set process.env directly.
   setupFiles: ['dotenv/config'],
   preset: 'ts-jest',
   testEnvironment: 'node',
   setupFilesAfterEnv: ['<rootDir>/src/__tests__/setup.ts'],
   
-  // ** ADDED THIS LINE **
-  // A list of paths to directories that Jest should NOT search for files in.
-  // This prevents the "duplicate manual mock" warning by ignoring the compiled JS output from 'tsc'.
   modulePathIgnorePatterns: ['<rootDir>/dist/'],
 
   testMatch: [
@@ -38,26 +35,3 @@ module.exports = {
     'node_modules/(?!(mongodb-memory-server)/)'
   ]
 };
-
-
-// Add to package.json (devDependencies section)
-/*
-"devDependencies": {
-  "@types/jest": "^29.5.0",
-  "@types/supertest": "^6.0.0",
-  "jest": "^29.5.0",
-  "mongodb-memory-server": "^8.12.0",
-  "supertest": "^6.3.0",
-  "ts-jest": "^29.1.0"
-}
-*/
-
-// Add to package.json (scripts section)
-/*
-"scripts": {
-  "test": "NODE_ENV=test jest --runInBand",
-  "test:watch": "NODE_ENV=test jest --watch",
-  "test:coverage": "NODE_ENV=test jest --coverage",
-  "test:verbose": "NODE_ENV=test jest --verbose"
-}
-*/
